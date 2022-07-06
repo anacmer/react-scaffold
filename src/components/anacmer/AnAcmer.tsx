d92473184efe7097701691b08a6c8aee07c67f4d
zhaojq <1179492305@qq.com>
2022-07-06 23:35:51 +0800

    Hello, React Scaffold!

---
+++ src/components/anacmer/AnAcmer.tsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import './AnAcmer.css'
+
+export default class AnAcmer extends Component {
+    render() {
+        return (
+            <h1 className="AnAcmer-background"> AnAcmer </h1>
+        )
+    }
+}
